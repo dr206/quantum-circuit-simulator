b49bfac9142a096625332a9a3d78e41d01ecee33
Daniel Reilly <daniel.reilly06@live.ie>
2021-02-07 15:16:21 +0000

    Add setup.py for module definition

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+setup(
+    name='quantumcircuitsimulator',
+    packages=find_packages(include=['quantumcircuitsimulator']),
+    version='0.1.0',
+    description='A circuit based quantum simulator.',
+    author='Daniel Reilly',
+    install_requires=['numpy'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+)
